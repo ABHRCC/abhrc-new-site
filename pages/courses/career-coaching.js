dcc8beb476c8d768dae4af999c1e4c2731c0bdfe
ABHRCC <info@abhrc.org>
2025-07-05 23:10:43 +0300

    Delete file

--- pages/courses/career-coaching.js
+++ pages/courses/career-coaching.js
@@ -1,15 +1,1 @@
 
-import Head from 'next/head';
-export default function CareerCoaching() {
-  return (
-    <>
-      <Head>
-        <title>Career Coaching</title>
-      </Head>
-      <main style={ padding: 30 }>
-        <h1>Career Coaching</h1>
-        <p>This is a detailed page for the "Career Coaching" course offered by ABHRC.</p>
-      </main>
-    </>
-  );
-}
